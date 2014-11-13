3905cd4b01606ab27efd6697b72551b0d71837bd
Andy Zhang <andzhng@gmail.com>
2014-11-13 14:50:10 -0800

    Moved levels, basic files set up

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 var express = require('express');
+var jade = require('jade');
+var path = require ('path');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/levels'));
 
 app.get('/', function(req, res) {
-  res.send('hello world');
+  console.log("Requesting from /");
+  res.send('index');
+});
+
+app.get('/levels', function(req, res) {
+  console.log("Requesting from levels");
+  res.send('pages');
+});
+
+app.get('/game', function(req, res) {
+  console.log("Requesting from game");
+  var levelId = req.query.id;
+  console.log(levelId || "No level defined");
+  res.send('game', { id: levelId }, function(err, html) {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
 
 app.listen(3000);
