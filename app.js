4aafcc92b88aab6eaa71b9f2f915ca4699003136
Andy Zhang <andzhng@gmail.com>
2014-11-13 15:10:06 -0800

    Fixed up the static files, serving the right things now

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
+app.set('views', __dirname + "/public/views/");
 app.engine('jade', require('jade').__express);
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(__dirname + '/public'));
@@ -13,23 +14,19 @@ app.use(express.static(__dirname + '/levels'));
 
 app.get('/', function(req, res) {
   console.log("Requesting from /");
-  res.send('index');
+  res.render('index');
 });
 
 app.get('/levels', function(req, res) {
   console.log("Requesting from levels");
-  res.send('pages');
+  res.render('pages');
 });
 
 app.get('/game', function(req, res) {
   console.log("Requesting from game");
   var levelId = req.query.id;
   console.log(levelId || "No level defined");
-  res.send('game', { id: levelId }, function(err, html) {
-    if (err) {
-      console.error(err);
-    }
-  });
+  res.render('game', { id: levelId });
 });
 
 app.listen(3000);
