f5a3f8b68f9dea62477f0f8c4447dfd58f6627bc
shikevin <im@shikev.in>
2014-11-13 19:20:22 -0500

    added more libraries for facebook authentication

---
+++ public/js/facebook-login.js
@@ -0,0 +1,31 @@
+$(document).ready(function (){
+  $.fbInit('880043985340563');
+
+  // FB Connect action
+  $('#fb-connect').bind('click', function () {
+    $.fbConnect({'include': ['first_name', 'last_name', 'name', 'pic']}, function (fbSession) {
+      $('.not_authenticated').hide();
+      $('.authenticated').show();
+      $('#fb-first_name').html(fbSession.first_name);
+    });
+    return false;
+  });
+
+  // FB Logout action
+  $('#fb-logout').bind('click', function () {
+    $.fbLogout(function () {
+      $('.authenticated').hide();
+      $('.not_authenticated').show();
+    });
+    return false;
+  });
+  // Check whether we're logged in and arrange page accordingly
+  $.fbIsAuthenticated(function (fbSession) {
+    // Authenticated!
+    $('.authenticated').show();
+    $('#fb-first_name').html(fbSession.first_name);
+  }, function () {
+    // Not authenticated
+    $('.not_authenticated').show();
+  });
+});
