066f69c7048abf66d44c068f108912a910a7a63e
shikevin <im@shikev.in>
2014-11-14 04:05:16 -0500

    added switch

--- public/js/constants.js
+++ public/js/constants.js
@@ -2,9 +2,15 @@ var Tile = {
   PATH: " ",
   WALL: "X",
   WATER: "W",
+  SWITCH: "O",
   PLAYER_ONE: "1",
   PLAYER_TWO: "2"
 };
+
+var Switch = {
+  OFF: 0,
+  ON: 1
+}
 
 var Direction = {
   TOP: 0,
--- public/js/renderer.js
+++ public/js/renderer.js
@@ -9,6 +9,7 @@ var Renderer = function(dom, width, height) {
   // Define assets
   this.playerTexture = loadPlayerImages();
   this.playerAnimations = loadPlayerAnimations();
+  this.switchTexture = loadSwitchImages();
   this.tileImages = loadTileImages();
 
   // Define constants
@@ -26,9 +27,20 @@ Renderer.prototype.initializeTiles = function(map) {
   map.initializeTiles(this.tileContainer, this.tileImages);
 }
 
-Renderer.prototype.render = function(player1, player2) {
+Renderer.prototype.initializeSwitches = function(switches) {
+  console.log("Initializing switches");
+  for (var i = 0; i < switches.length; i++) {
+    switches[i].initSprite(this.stage, this.switchTexture, this.tileSize);
+  }
+}
+
+Renderer.prototype.render = function(player1, player2, switches) {
   player1.render(this.tileSize);
   player2.render(this.tileSize);
+/*  for (var i = 0; i < switches.length; i++) {
+    switches[i].render();
+  }*/
+
   this.renderer.render(this.stage);
 }
 
@@ -51,6 +63,13 @@ Renderer.prototype.animate = function(player1, player2) {
   player1.animate();
   player2.animate();
   this.renderer.render(this.stage);
+}
+
+function loadSwitchImages() {
+  var assets = {};
+  assets[Switch.ON] = PIXI.Texture.fromImage("/img/switch/on.png");
+  assets[Switch.OFF] = PIXI.Texture.fromImage("/img/switch/off.png");
+  return assets;
 }
 
 function loadPlayerImages() {
