07f858e516d0a5e673fd98a164bb6e7a9e9570d1
Gabriel Wong <me@gabrielwong.net>
2014-11-14 13:03:55 -0800

    fix level 2

--- public/js/controller.js
+++ public/js/controller.js
@@ -22,8 +22,8 @@
   Controller.prototype.parseGoal = function (goal, player1, player2) {
     for (key in goal) {
       var location = {};
-      location['column'] = goal[key][0];
-      location['row'] = goal[key][1];
+      location['row'] = goal[key][0];
+      location['column'] = goal[key][1];
       if (key == "1") {
         player1.setGoal(location);
       } else if (key == "2") {
